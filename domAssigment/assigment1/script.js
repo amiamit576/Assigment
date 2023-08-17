dd2304eed5e4541ca91732f1007ebe0a4ab71d4a
Amit <amiamit576@gmail.com>
2023-08-17 23:24:02 +0530

    dom assigment

---
+++ domAssigment/assigment1/script.js
@@ -0,0 +1,34 @@
+let count = 0;
+// select value and button
+ const value=document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+console.log(value);
+console.log(btns);
+btns.forEach(function(btn){
+    btn.addEventListener('click',function(e){
+        const styles =e.currentTarget.classList;
+        if(styles.contains('dec')){
+            count--;
+        }
+        else if(styles.contains('inc')){
+            count++;
+        }
+        else{
+            count=0
+        }
+        if(count>0){
+            value.style.color='green'
+        }
+        else if(count<0){
+            value.style.color='red'
+        }
+        if(count===0) {
+            value.style.color='#222' 
+        }
+        value.textContent=count;
+    })
+    
+
+})
+
+
